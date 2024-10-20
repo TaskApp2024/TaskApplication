d6864923ba4c99b91b63079d2dc0364543edaafe
muthu <muthukumarana469@gmail.com>
2024-10-20 16:38:17 +0530

    added another console log

--- backend/controller/company/companyController.js
+++ backend/controller/company/companyController.js
@@ -428,6 +428,7 @@ const displayEmployees = async (req, res) => {
       const employees = await Employee.find({ companyId: companyId });
 
       if (employees.length === 0) {
+        console.log("empty table -employess")
         return res.status(200).json({ message: 'Employees not Found' });
       }
 
